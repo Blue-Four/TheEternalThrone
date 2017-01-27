bc59c33332665e848c8aea4c07035d72c711ff24
Iriohm <AthelOrii@GMail.com>
2017-01-27 14:03:49 -0800

    Undid accidental reversion to restore Michael L's changes.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var AM = new AssetManager();
 AM.queueDownload("./img/background.jpg");
 AM.queueDownload("./img/barbarian_spritesheet.png");
 AM.queueDownload("./img/skeleton_spritesheet.png");
+AM.queueDownload("./img/villager1_spritesheet.png");
 AM.queueDownload("./img/zombie.png");
 
 AM.downloadAll(function () {
@@ -20,15 +21,27 @@ AM.downloadAll(function () {
 	var barbarianPC = new CharacterPC(gameEngine, AM.getAsset("./img/barbarian_spritesheet.png"), 200, 200);
     gameEngine.addEntity(barbarianPC);
 	
-	var swordyMcSwordface = new Enemy_Skeleton_Melee(gameEngine, AM.getAsset("./img/skeleton_spritesheet.png"), 300, 300);
-    gameEngine.addEntity(swordyMcSwordface);
-	apply_AI_Wander(swordyMcSwordface);
+	var missDemeanor = new Ally_Villager(gameEngine, AM.getAsset("./img/villager1_spritesheet.png"), 400, 200);
+    gameEngine.addEntity(missDemeanor);
+	apply_AI_Wander(missDemeanor);
 	
-	for(var i = 0; i <= 10; i++) {
-		var zombie = new Zombie(gameEngine, AM.getAsset("./img/zombie.png"));
+	for(var i = 0; i < 4; i++) {	
+		//spawn randomly for display purposes
+		rX = Math.floor(Math.random() * 800);
+   		rY = Math.floor(Math.random() * 600);
+		var zombie = new Zombie(gameEngine, AM.getAsset("./img/zombie.png"), rX, rY);
 		gameEngine.addEntity(zombie);
+		apply_AI_Wander(zombie);
 	}
-	
+
+	for(var i = 0; i < 4; i++) {
+		rX = Math.floor(Math.random() * 800);
+   		rY = Math.floor(Math.random() * 600);
+		var swordyMcSwordface = new Enemy_Skeleton_Melee(gameEngine, AM.getAsset("./img/skeleton_spritesheet.png"), rX, rY);
+	    gameEngine.addEntity(swordyMcSwordface);
+		apply_AI_Wander(swordyMcSwordface);
+	}
+
     console.log("All Done!");
 	
 });
