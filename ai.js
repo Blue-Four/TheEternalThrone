bc8bc1ec70ecbaabd6cdf78f916258cf58f3fbfd
Iriohm <athelorii@gmail.com>
2017-02-04 15:20:11 -0800

    Test Changes - In the middle of implementing pathfinding

--- ai.js
+++ ai.js
@@ -16,3 +16,64 @@ function apply_AI_Wander(character) {
 	}, 5000 + (Math.floor(Math.random() * 2000) - 1000));
 	
 }
+
+// function PathNode(x, y) {
+	// this.x = x;
+	// this.y = y;
+	// this.next = null;
+	
+// }
+
+// function findPath(startX, startY, endX, endY, walkableTerrain) {
+	// var x = Math.round(startX / 48)	* 48;
+	// var y = Math.round(startX / 24)	* 24;
+	// var start_node = new PathNode(x, y);
+	
+	
+	
+	// return start_node;
+	
+// }
+
+// function mapWalkability(image, image_x, image_y) {
+    // var offscreenCanvas = document.createElement('canvas');
+    // offscreenCanvas.width = image_x;
+    // offscreenCanvas.height = image_y;
+    // var offscreenCtx = offscreenCanvas.getContext('2d');
+    // offscreenCtx.save();
+    // offscreenCtx.drawImage(image, 0, 0);
+    // offscreenCtx.restore();
+	
+	// var pixels = offscreenCtx.getImageData(0, 0, image_x, image_y).data;
+	
+	// var overArray = new Array();
+	
+	// for	(var outer_count = 0; outer_count < image_x; outer_count++) {
+		// overArray[outer_count] = new Array();
+		
+		// for	(var inner_count = 0; inner_count < (image_y * 2); inner_count++) {
+			// overArray[outer_count][inner_count] = ;
+			
+		// }
+		
+		
+	// }
+	
+// }
+
+// Calculates the percentage of opaque pixels within a single, 48 by 24 square.
+function computeSquare(pixel_data, x, y, image_width, image_height) {
+	var opaquePixels = 0;
+	var sensitivity = 1;
+	
+	for	(var currentX = x; currentX < x + 48; currentX = currentX + sensitivity) {
+		for	(var currentY = y; currentY < y + 24; currentY = currentY + sensitivity) {
+			if	(pixel_data[(currentX + (currentY * image_width)) * 4 + 3] === 255) { opaquePixels++; }
+			
+		}
+		
+	}
+	
+	return opaquePixels / (1152 / sensitivity);
+	
+}
--- main.js
+++ main.js
@@ -15,40 +15,53 @@ AM.downloadAll(function () {
     gameEngine.init(ctx);
     gameEngine.start();
 	
-	var background = new Background(gameEngine, AM.getAsset("./img/background.jpg"));
+	var background = new Background(gameEngine, AM.getAsset("./img/background.jpg"), AM.getAsset("./img/background.jpg"));
     gameEngine.addEntity(background);
-
-	var barbarianPC = new CharacterPC(gameEngine, AM.getAsset("./img/barbarian_spritesheet.png"), 200, 200, 60, 175, 1);
-    gameEngine.addEntity(barbarianPC);
 	
-	var missDemeanor = new Ally_Villager(gameEngine, AM.getAsset("./img/villager1_spritesheet.png"), 400, 200, 60, 110, 1);
-    gameEngine.addEntity(missDemeanor);
-	apply_AI_Wander(missDemeanor);
+	var background = AM.getAsset("./img/barbarian_spritesheet.png");
+	var offscreenCanvas = document.createElement('canvas');
+    offscreenCanvas.width = 400;
+    offscreenCanvas.height = 400;
+    var offscreenCtx = offscreenCanvas.getContext('2d');
+    offscreenCtx.save();
+    offscreenCtx.drawImage(background, 0, 0);
+    offscreenCtx.restore();
 	
-	for(var i = 0; i < 4; i++) {	
-		//spawn randomly for display purposes
-		rX = Math.floor(Math.random() * 800);
-   		rY = Math.floor(Math.random() * 600);
-		var zombie = new Zombie(gameEngine, AM.getAsset("./img/zombie.png"), rX, rY);
-		gameEngine.addEntity(zombie);
-		apply_AI_Wander(zombie);
-	}
+	var pixels = offscreenCanvas.getContext('2d').getImageData(0,0, 128, 128).data;
+	var percentage = computeSquare(pixels, 55, 55, 128, 128);
+	console.log("Percentage is " + percentage);
 
-	for(var i = 0; i < 4; i++) {
-		rX = Math.floor(Math.random() * 800);
-   		rY = Math.floor(Math.random() * 600);
-		var swordyMcSwordface = new Enemy_Skeleton_Melee(gameEngine, AM.getAsset("./img/skeleton_spritesheet.png"), rX, rY, 60, 110, 1);
-	    gameEngine.addEntity(swordyMcSwordface);
-		apply_AI_Wander(swordyMcSwordface);
-	}
+	// var barbarianPC = new CharacterPC(gameEngine, AM.getAsset("./img/barbarian_spritesheet.png"), 200, 200, 60, 175, 1);
+    // gameEngine.addEntity(barbarianPC);
+	
+	// var missDemeanor = new Ally_Villager(gameEngine, AM.getAsset("./img/villager1_spritesheet.png"), 400, 200, 60, 110, 1);
+    // gameEngine.addEntity(missDemeanor);
+	// apply_AI_Wander(missDemeanor);
+	
+	// for(var i = 0; i < 4; i++) {	
+		// //spawn randomly for display purposes
+		// rX = Math.floor(Math.random() * 800);
+   		// rY = Math.floor(Math.random() * 600);
+		// var zombie = new Zombie(gameEngine, AM.getAsset("./img/zombie.png"), rX, rY);
+		// gameEngine.addEntity(zombie);
+		// apply_AI_Wander(zombie);
+	// }
 
-	for(var i = 0; i < 2; i++) {
-		rX = Math.floor(Math.random() * 800);
-   		rY = Math.floor(Math.random() * 600);
-		var largeMcSwordface = new Large_Skeleton_Melee(gameEngine, AM.getAsset("./img/skeleton_spritesheet.png"), rX, rY, 60, 25, 2);
-	    gameEngine.addEntity(largeMcSwordface);
-	    apply_AI_Wander(largeMcSwordface);
-	}
+	// for(var i = 0; i < 4; i++) {
+		// rX = Math.floor(Math.random() * 800);
+   		// rY = Math.floor(Math.random() * 600);
+		// var swordyMcSwordface = new Enemy_Skeleton_Melee(gameEngine, AM.getAsset("./img/skeleton_spritesheet.png"), rX, rY, 60, 110, 1);
+	    // gameEngine.addEntity(swordyMcSwordface);
+		// apply_AI_Wander(swordyMcSwordface);
+	// }
+
+	// for(var i = 0; i < 2; i++) {
+		// rX = Math.floor(Math.random() * 800);
+   		// rY = Math.floor(Math.random() * 600);
+		// var largeMcSwordface = new Large_Skeleton_Melee(gameEngine, AM.getAsset("./img/skeleton_spritesheet.png"), rX, rY, 60, 25, 2);
+	    // gameEngine.addEntity(largeMcSwordface);
+	    // apply_AI_Wander(largeMcSwordface);
+	// }
 
     console.log("All Done!");
 	
